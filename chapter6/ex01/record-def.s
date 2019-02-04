6259bbf41b857302d2f2df6b23e9cdd972a29c7e
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2019-02-04 13:21:43 +0100

    Create record-def.s

---
+++ chapter6/ex01/record-def.s
@@ -0,0 +1,7 @@
+.equ RECORD_FIRSTNAME, 0
+.equ RECORD_LASTNAME, 40
+.equ RECORD_ADDRESS, 80
+.equ RECORD_PHONE, 320          # 40 byte for the phone number
+.equ RECORD_AGE, 360
+
+.equ RECORD_SIZE, 364
