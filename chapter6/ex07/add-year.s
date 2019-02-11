5c83576b4ae0310fdc6e606ca32f5b889cf267ec
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2019-02-11 11:07:53 +0100

    Create add-year.s

---
+++ chapter6/ex07/add-year.s
@@ -0,0 +1,126 @@
+.include "linux.s"
+.include "record-def.s"
+.section .data
+input_file_name:
+.ascii "test.dat\0"
+
+output_file_name:
+.ascii "testout.dat\0"
+
+error_open_in:
+.ascii "Error while opening test.dat: \0"
+
+error_open_out:
+.ascii "Error while opening testout.dat: \0"
+
+error_readr:
+.ascii "Error while reading record: \0"
+
+error_writer:
+.ascii "Error while writing record: \0"
+
+.section .bss
+.lcomm record_buffer, RECORD_SIZE
+
+#Stack offsets of local variables
+.equ ST_INPUT_DESCRIPTOR, -4
+.equ ST_OUTPUT_DESCRIPTOR, -8
+
+.section .text
+.globl _start
+_start:
+#Copy stack pointer and make room for local variables
+movl  %esp, %ebp
+subl  $8, %esp
+
+#Open file for reading
+movl  $SYS_OPEN, %eax
+movl  $input_file_name, %ebx
+movl  $0, %ecx
+movl  $0666, %edx
+int   $LINUX_SYSCALL
+cmpl  $0, %eax
+jl    exit_err_open_in
+
+movl  %eax, ST_INPUT_DESCRIPTOR(%ebp)
+
+#Open file for writing
+movl  $SYS_OPEN, %eax
+movl  $output_file_name, %ebx
+movl  $0101, %ecx
+movl  $0666, %edx
+int   $LINUX_SYSCALL
+cmpl  $0, %eax
+jl    exit_err_open_out
+
+movl  %eax, ST_OUTPUT_DESCRIPTOR(%ebp)
+
+loop_begin:
+pushl ST_INPUT_DESCRIPTOR(%ebp)
+pushl $record_buffer
+call  read_record
+addl  $8, %esp
+cmpl  $0, %eax
+jl    exit_err_readr
+
+#Returns the number of bytes read.
+#If it isn't the same number we
+#requested, then it's either an
+#end-of-file, or an error, so we're
+#quitting
+cmpl  $RECORD_SIZE, %eax
+jne   loop_end
+
+#Increment the age
+incl  record_buffer + RECORD_AGE
+
+#Write the record out
+pushl ST_OUTPUT_DESCRIPTOR(%ebp)
+pushl $record_buffer
+call  write_record
+addl  $8, %esp
+cmpl  $0, %eax
+jl    exit_err_writer
+
+jmp   loop_begin
+
+loop_end:
+movl  $SYS_EXIT, %eax
+movl  $0, %ebx
+int   $LINUX_SYSCALL
+
+exit_err_writer:
+pushl $error_writer
+pushl %eax
+call  print_error
+addl  $8, %esp
+movl  %eax, %ebx
+movl  $SYS_EXIT, %eax
+int   $LINUX_SYSCALL
+
+exit_err_readr:
+pushl $error_readr
+pushl %eax
+call  print_error
+addl  $8, %esp
+movl  %eax, %ebx
+movl  $SYS_EXIT, %eax
+int   $LINUX_SYSCALL
+
+exit_err_open_in:
+pushl $error_open_in
+pushl %eax
+call  print_error
+addl  $8, %esp
+movl  %eax, %ebx
+movl  $SYS_EXIT, %eax
+int   $LINUX_SYSCALL
+
+exit_err_open_out:
+pushl $error_open_out
+pushl %eax
+call  print_error
+addl  $8, %esp
+movl  %eax, %ebx
+movl  $SYS_EXIT, %eax
+int   $LINUX_SYSCALL
