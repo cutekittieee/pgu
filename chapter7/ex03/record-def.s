6ef6affd06effefd619541ad8b1e355b7fd16119
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2019-02-28 10:32:31 +0100

    Create record-def.s

---
+++ chapter7/ex03/record-def.s
@@ -0,0 +1,13 @@
+#Field offsets
+.equ RECORD_FIRSTNAME, 0
+.equ RECORD_LASTNAME, 40
+.equ RECORD_ADDRESS, 80
+.equ RECORD_AGE, 320
+
+.equ RECORD_SIZE, 324
+
+#Field lengths
+.equ LEN_FIRSTNAME, 40
+.equ LEN_LASTNAME, 40
+.equ LEN_ADDRESS, 240
+.equ LEN_AGE, 4
