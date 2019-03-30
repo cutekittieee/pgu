5b6045b902a4b80ccd5ef80538d44839f255ae81
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2019-03-30 09:01:02 +0100

    Create helloworld-lib.s

---
+++ chapter8/helloworld-lib.s
@@ -0,0 +1,19 @@
+#PURPOSE:  This program writes the message "hello world" and
+#          exits
+#
+
+.section .data
+
+helloworld:
+.ascii "hello world\n\0"
+
+.section .text
+.globl _start
+_start:
+pushl $helloworld
+call  printf
+addl  $4, %esp
+
+pushl $0
+call  exit
+addl  $4, %esp
